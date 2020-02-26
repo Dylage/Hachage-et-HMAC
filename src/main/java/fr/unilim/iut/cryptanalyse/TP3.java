60d5afc4a7cdd24feb594bdc79f2fc7015d301fa
Quentin DELAGE <quentin.delage1@etu.unilim.fr>
2020-02-26 13:47:12 +0100

    Menu pour le main

--- src/main/java/fr/unilim/iut/cryptanalyse/TP3.java
+++ src/main/java/fr/unilim/iut/cryptanalyse/TP3.java
@@ -9,12 +9,59 @@ public class TP3 {
 
     public static void main(String[] args) {
 
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Entrez le message à hacher");
-        // String msg = sc.nextLine();
+        Scanner sc = new Scanner(System.in);
 
-        // System.out.println(Hachage.hachage("test"));
+        System.out.println("Bienvenue dans ce module de hachage");
+        System.out.println("1 - Hacher");
+        System.out.println("2 - Trouver collisions");
+        // System.out.println("3 - Cryptanalyse (Kasiski)");
+        System.out.print("Votre choix : ");
 
-        Hachage.findCollision("ods5.txt");
+        int choix = sc.nextInt();
+
+        String clearText;
+        byte[] hashedText;
+
+        
+
+        switch (choix) {
+            case 1:
+                System.out.println("Veuillez entrer le texte à hacher :");
+                clearText = sc.next();
+        
+                hashedText = Hachage.hachage(clearText);
+                
+                System.out.println("Texte haché : " + hashedText);
+                break;
+            case 2:
+                System.out.println("Veuillez entrer le nombre de bits (entre 1 et 16) :");
+
+                int nbBits = sc.nextInt();
+
+                if (nbBits > 16) {
+                    System.err.println("Trop de bits pour le programme...");
+                    break;
+                }
+
+                Hachage.findCollision("ods5.txt", nbBits);
+                        
+                break;
+            case 3:
+                // System.out.println("Veuillez entrer le texte à analyser :");
+                // crypted = sc.next();
+                // int[] tailles = Kasiski.estimateKeySize(crypted);
+                // System.out.println("\nTaille probable de la clef : " + tailles[0] + " ou " + tailles[1]);
+
+                break;
+        
+            default:
+                System.err.println("Mauvaise proposition !");
+                break;
+        }
+
+        System.out.println("\n\nÀ votre service\nAu revoir...");
+        sc.close();
+
     }
+
 }
