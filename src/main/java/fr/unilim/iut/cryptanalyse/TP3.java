392f55beb27756a5e197a079c9d8056806fd2b84
Quentin DELAGE <quentin.delage1@etu.unilim.fr>
2020-02-26 15:19:05 +0100

    Menu texte à hacher HMAC + saisie plusieurs mots

--- src/main/java/fr/unilim/iut/cryptanalyse/TP3.java
+++ src/main/java/fr/unilim/iut/cryptanalyse/TP3.java
@@ -15,11 +15,14 @@ public class TP3 {
         System.out.println("1 - Hacher");
         System.out.println("2 - Trouver collisions");
         System.out.println("3 - Trouver collisions avec un mot");
+        System.out.println("4 - Hacher un mot avec une clef (HMAC-SHA1)");
         System.out.print("Votre choix : ");
 
         int choix = sc.nextInt();
+        sc.nextLine();
 
-        String clearText;
+        String clearText = "";
+        String key = "";
         byte[] hashedText;
         int nbBits;
         
@@ -27,7 +30,7 @@ public class TP3 {
         switch (choix) {
             case 1:
                 System.out.println("Veuillez entrer le texte à hacher :");
-                clearText = sc.next();
+                clearText = sc.nextLine();
         
                 hashedText = Hachage.hachage(clearText);
                 
@@ -50,7 +53,7 @@ public class TP3 {
 
                 System.out.println("Veuillez entrer le mot à chercher");
 
-                String word = sc.next();
+                String word = sc.nextLine();
 
                 System.out.println("Veuillez entrer le nombre de bits (entre 1 et 16) :");
 
@@ -64,7 +67,21 @@ public class TP3 {
                 Hachage.findCollisionWithHashed("ods5.txt", word, nbBits);
 
                 break;
-        
+                
+            case 4:
+                System.out.println("Veuillez saisir le texte à hacher :");
+                clearText = sc.nextLine();
+
+                System.out.println("le texte est : " + clearText);
+
+                System.out.println("Veuillez saisir la clef :");
+                key = sc.nextLine();
+
+                hashedText = HMAC.hmac(clearText, key);
+
+                System.out.println(HMAC.toHex(hashedText));
+
+                break;
             default:
                 System.err.println("Mauvaise proposition !");
                 break;
